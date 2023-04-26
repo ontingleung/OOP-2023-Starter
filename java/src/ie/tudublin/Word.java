149ef565c436f130f0d763cba4256059b7e8e841
ontingleung <ontingleung@tutamail.com>
2023-04-26 16:41:03 +0100

    word class

--- java/src/ie/tudublin/Word.java
+++ java/src/ie/tudublin/Word.java
@@ -1,5 +1,15 @@
+//Make a class called Word that has fields for the word (a String) and an ArrayList of type Follow as fields. 
+//Make accessor methods, a constructor and a toString Method that will iterate through the follows ArrayList. (See the printout of the model above).
+
 package ie.tudublin;
 
+import java.util.ArrayList;
+
 public class Word {
-    
+    private String word;
+    private ArrayList<Follow> follows;
+
+    public String getWord() {
+        return word;
+    }
 }
